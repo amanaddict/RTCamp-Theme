57ddccbcaf210951c85045e6502fbabeeee06e8d
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-08 03:54:30 +0530

    stylesheet modified and dynamically added using functions.php file

--- wp-content/themes/blogweb/functions.php
+++ wp-content/themes/blogweb/functions.php
@@ -2,7 +2,7 @@
 
 function followandrew_register_styles(){
     wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/style.css", array(), '1.0', 'all'); 
-    wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh", array(), '1.0', 'all');
+    wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0', 'all');
     wp_enqueue_style('followandrew-fontawesome',  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
 }
 
