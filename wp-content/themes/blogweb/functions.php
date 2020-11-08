5f33b465dbe465c87c632920a4b84f870bb11efc
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-09 01:44:33 +0530

    functions file modified with dyn. content

--- wp-content/themes/blogweb/functions.php
+++ wp-content/themes/blogweb/functions.php
@@ -15,10 +15,7 @@ function followandrew_register_scripts(){
     wp_enqueue_script('followandrew-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1',true);
 }
 
-function followandrew_register_scripts(){
 add_action('wp_enqueue_scripts', 'followandrew_register_scripts');
-
-
 
 
 
