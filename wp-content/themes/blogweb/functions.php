98bf6b1f5ed1114c6fe923ff9c4ddf9f7d5dec1e
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-14 01:33:06 +0530

    modified

--- wp-content/themes/blogweb/functions.php
+++ wp-content/themes/blogweb/functions.php
@@ -17,8 +17,9 @@ function followandrew_register_styles(){
     wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('followandrew-fontawesome',  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
-
+//Hook for the above registered function
 add_action('wp_enqueue_scripts', 'followandrew_register_styles' ); /*Registering the  scripts*/
+//Working-> It means that whenever 'wp_enqueue_scripts' functionis executed then execute the 'followandrew_register_styles' also. 
 
 /*for js and jquery loading*/
 function followandrew_register_scripts(){
