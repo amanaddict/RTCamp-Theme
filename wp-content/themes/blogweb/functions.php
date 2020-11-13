d643dd580920d34102464bd4aa79f35bf73eba9a
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-14 01:27:23 +0530

    modified

--- wp-content/themes/blogweb/functions.php
+++ wp-content/themes/blogweb/functions.php
@@ -13,7 +13,7 @@ add_action('after_setup_theme','followandrew_theme_support');
 function followandrew_register_styles(){
     $version = wp_get_theme()->get('Version'); /*dynamically getting the version*/
 
-    wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/style.css", array('followandrew-bootstrap'), $version, 'all'); 
+    wp_enqueue_style('followandrew-style', get_template_directory_uri() . "ss/style.css", array('followandrew-bootstrap'), $version, 'all'); 
     wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
     wp_enqueue_style('followandrew-fontawesome',  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
