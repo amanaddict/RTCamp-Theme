f7104466a2f5cda05397f683be11d22486afd870
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-14 15:52:22 +0530

    modified

--- wp-content/themes/blogweb/footer.php
+++ wp-content/themes/blogweb/footer.php
@@ -1,8 +1,16 @@
+
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+           <p class="copyright"><a href="http://divyankportfolio.000webhostapp.com">Divyank Kargeti</a></p>
+          
+       </footer>
+   
+   </div>
+
 <!-- Bootstrap Javascript -->   
 <?php
     wp_footer();
 ?>
-	
 
 </body>
 </html> 
--- wp-content/themes/blogweb/header.php
+++ wp-content/themes/blogweb/header.php
@@ -62,5 +62,5 @@
 	</header>
 	<div class="main-wrapper">
 	    <header class="page-title theme-bg-light text-center gradient py-5">
-			<h1 class="heading">Blogging Home Page Heading Goes Here!!</h1>
+			<h1 class="heading"><?php the_title(); ?></h1>
 		</header>
