7a1e8eb7ed7bda0751b49e0a89a9723b64b9c566
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-16 04:36:56 +0530

    modified

--- wp-content/themes/blogweb/template-parts/content-archive.php
+++ wp-content/themes/blogweb/template-parts/content-archive.php
@@ -3,17 +3,20 @@
 <div class="container">
     <div class="post mb-5">
 	    <div class="media">
+            <?
+            
+            ?>
 		    <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="images/thumbs/1.jpg" alt="image">
 		    <div class="media-body">
 			    <h3 class="title mb-1"><a href="post.html"><?php the_title(); ?></a></h3>
-			    <div class="meta mb-1"><span class="date">Published on: <?php the_date(); ?> </span><span class="time">5 min read</span><span class="comment"><a href="#">8 comments</a></span></div>
+			    <div class="meta mb-1"><span class="date">Published on: <?php the_date(); ?> </span><span class="comment"><a href="#">|| <?php comments_number(); ?> </a></span></div>
 			    <div class="intro">  
                     <!--Loading up excerpts of archive posts from archive.php file-->
                     <?php 
                        the_excerpt(); 
                     ?>
                 </div>
-				    <a class="more-link" href="post.html">Read more &rarr;</a>
+				    <a class="more-link" href="<?php the_permalink(); ?>">Read more &rarr;</a>
 			</div><!--//media-body-->
 		</div><!--//media-->
     </div>
