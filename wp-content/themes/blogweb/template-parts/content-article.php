d0ef1117d70c208767eb7b863bde8899f9d109c1
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-16 02:12:25 +0530

    modified

--- wp-content/themes/blogweb/template-parts/content-article.php
+++ wp-content/themes/blogweb/template-parts/content-article.php
@@ -6,7 +6,12 @@
 <div class="container">
     
     <header class="content-header">
-		<div class="meta mb-3"><span class="date">Published 3 months ago</span><span class="tag"><i class='fa fa-tag'></i> tag</span><span class="tag"><i class='fa fa-tag'></i> category</span><span class="comment"><a href="#comments"><i class='fa fa-comment'></i> 3 comments</a></span></div>
+		<div class="meta mb-3">
+            <span class="date"> <b>Posted on:</b> <?php the_date(); ?> </span>
+            <span class="tag"><i class='fa fa-tag'></i> tag</span>
+            <span class="tag"><i class='fa fa-tag'></i> category</span>
+            <span class="comment"><a href="#comments"><i class='fa fa-comment'></i> 3 comments</a></span>
+        </div>
 	</header>
 
    <?php 
