818f59d4c9bad7eb85c7025fa851d2528111c8f3
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-16 04:10:33 +0530

    modified

---
+++ wp-content/themes/blogweb/template-parts/content-archive.php
@@ -0,0 +1,12 @@
+<!--content-archive.php file-->
+<!--This file is checked for archive type of posts-->
+
+
+<div class="container">
+    
+    <!--Loading up content(i.e archive post) from archive.php file-->
+   <?php 
+       the_content(); 
+    ?>
+
+</div>
