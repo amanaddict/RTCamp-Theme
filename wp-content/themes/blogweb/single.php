7943d6b29f64c983d127e94bb4a7f8dc79f09b2c
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-16 01:10:47 +0530

    modified

--- wp-content/themes/blogweb/single.php
+++ wp-content/themes/blogweb/single.php
@@ -13,7 +13,8 @@ get_header();
 				if ( have_posts() ) {
 					while( have_posts() ){
 						the_post();
-						the_content(); //gets the content
+                        
+                        get_template_part('template-parts/content'); //checks the type of post from template-parts folder.
 					}
 				}
 			?>
