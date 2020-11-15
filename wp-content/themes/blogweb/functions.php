2213ef87d93d284aedebd2f5c0804110a4d851e5
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-16 00:44:00 +0530

    modified

--- wp-content/themes/blogweb/functions.php
+++ wp-content/themes/blogweb/functions.php
@@ -5,6 +5,7 @@
 function followandrew_theme_support(){
     add_theme_support('title-tag'); // for custom title
     add_theme_support('custom-logo'); // for custom logo/img
+    add_theme_support('post-thumbnails'); // 
 }
 add_action('after_setup_theme','followandrew_theme_support');
 
