3d1dd465e70de5a55105fe0dc058fa2c07a960c5
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-16 03:17:42 +0530

    modified

--- wp-content/themes/blogweb/comments.php
+++ wp-content/themes/blogweb/comments.php
@@ -1,3 +1,7 @@
+<!--comments.php-->
+<!--This file is meant for comments and replies functionality-->
+
+
 <div class="comments-wrapper">
 
 
@@ -13,7 +17,7 @@
                                         echo "No Comments, Kindly add one!";                       
                                      }
                                      else{
-                                        echo get_comments_count(). "Comments";
+                                        echo get_comments_number(). "Comments";
                                      }
                                 ?>
 
@@ -24,23 +28,32 @@
 
 						<div class="comments-inner">
                         
-                        <!--FOr each and every individual comments-->
+                        <!--For each and every individual comments-->
                         <?php
                             wp_list_comments(
+                                array(
+                                  'avatar_size' => 120,
+                                  'style' => 'div',
+                                )
                             ); 
                         ?>
-                            
-                        
 
 						</div><!-- .comments-inner -->
 
 					</div><!-- comments -->
 
 					<hr class="" aria-hidden="true">
-					<div id="respond" class="comment-respond">
-						<h2 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow"
-									id="cancel-comment-reply-link" href="/?p=1#respond" style="display:none;">Cancel
-									reply</a></small></h2>
-					</div><!-- #respond -->
+                    <!--Response comment-->
+                    <?php
+                        if( comments_open() ){
+                            comment_form(
+                                array(
+                                    'class_form' => '',
+                                    'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+                                    'title_reply_after' => '</h2>',
+                                )
+                            );
+                        }
+                    ?>
 
 				</div>
