59fdca615e5a80dcba0e887325b88bc7abe85980
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-16 00:51:11 +0530

    modified

---
+++ wp-content/themes/blogweb/single.php
@@ -0,0 +1,30 @@
+<!--single.php-->
+<!--This file is meant for single posts-->
+
+<!--Including header or header.php file using wordpress php function-->
+<?php
+get_header();
+?> 
+<!---->   
+    
+		<article class="content px-3 py-5 p-md-5">
+			<!--
+				WordPress Loop: It is an iterator that requests DB and gets the data from there
+			-->
+			<?php
+				if ( have_posts() ) {
+					while( have_posts() ){
+						the_post();
+						the_content(); //gets the content
+					}
+				}
+			?>
+	    </article>
+	    
+    
+    <!--Including footer using wordpress php function-->
+   <?php
+      get_footer();
+   ?>  
+   <!---->
+
